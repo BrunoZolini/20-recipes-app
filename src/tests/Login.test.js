a3cb26065563cfae94158dc26bc3465971a27a85
viniciusopassos <viniciusopassos@hotmail.com>
2022-03-28 17:48:37 -0300

    req-3: teste input email

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 // import App from '../App';
+import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
 
 describe('2 - Crie um componente chamado `Login`', () => {
@@ -19,5 +20,15 @@ describe('2 - Crie um componente chamado `Login`', () => {
       data-testid="login-submit-btn"`, () => {
     render(<Login />);
     expect(screen.getByTestId('login-submit-btn')).toBeInTheDocument();
+  });
+});
+
+describe('3 - Verificar se é possível escrever no "input" de email', () => {
+  it(`Será validado se existe uma 'input' que possui o 
+      data-testid="email-input"`, () => {
+    render(<Login />);
+    const inputEmail = screen.getByTestId('email-input');
+    userEvent.type(inputEmail, 'teste@teste.com');
+    expect(inputEmail).toHaveValue('teste@teste.com');
   });
 });
