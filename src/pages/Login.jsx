173e4d24d04dda47c577652fad8eba537739e92c
viniciusopassos <viniciusopassos@hotmail.com>
2022-03-28 19:13:15 -0300

    req-5: função p/ desabilitar botão e testes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const SIX = 6;
+    if (password.length > SIX && emailRegex.test(email)) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [email, password]);
+
   return (
     <div>
       <form>
-        <input data-testid="email-input" placeholder="email" type="email" />
-        <input data-testid="password-input" type="password" placeholder="password" />
-        <button type="submit" data-testid="login-submit-btn">Enter</button>
+        <input
+          data-testid="email-input"
+          placeholder="email"
+          type="email"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="password"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ isDisabled }
+        >
+          Enter
+        </button>
       </form>
     </div>
   );
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-// import App from '../App';
 import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
 
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+const SUBMIT_BUTTON = 'login-submit-btn';
+
 describe('2 - Crie um componente chamado `Login`', () => {
-  it('Será validado se existe uma `input` que possui o data-testid="email-input"', () => {
+  it('Será validado se existe uma `input` que possui o data-testid', () => {
     render(<Login />);
-    expect(screen.getByTestId('email-input')).toBeInTheDocument();
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    expect(inputEmail).toBeInTheDocument();
   });
 
   it(`Será validado se existe uma 'input' que possui o 
       data-testid="password-input"`, () => {
     render(<Login />);
-    expect(screen.getByTestId('password-input')).toBeInTheDocument();
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    expect(inputPassword).toBeInTheDocument();
   });
 
   it(`Será validado se existe um 'button' que possui o 
       data-testid="login-submit-btn"`, () => {
     render(<Login />);
-    expect(screen.getByTestId('login-submit-btn')).toBeInTheDocument();
+    const submitButton = screen.getByTestId(SUBMIT_BUTTON);
+    expect(submitButton).toBeInTheDocument();
   });
 });
 
@@ -27,18 +33,35 @@ describe('3 - Verificar se é possível escrever no "input" de email', () => {
   it(`Será validado se existe uma 'input' que possui o 
       data-testid="email-input"`, () => {
     render(<Login />);
-    const inputEmail = screen.getByTestId('email-input');
-    userEvent.type(inputEmail, 'teste@teste.com');
-    expect(inputEmail).toHaveValue('teste@teste.com');
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    userEvent.type(inputEmail, 'teste1@teste.com');
+    expect(inputEmail).toHaveValue('teste1@teste.com');
   });
 });
 
 describe('4 - Verificar se é possível escrever no "input" de senha', () => {
-  it(`Será validado se existe uma 'input' que possui o 
+  it(`Será validado se existe um 'input' que possui o 
       data-testid="password-input"`, () => {
     render(<Login />);
-    const inputSenha = screen.getByTestId('password-input');
-    userEvent.type(inputSenha, '123456');
-    expect(inputSenha).toHaveValue('123456');
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    userEvent.type(inputPassword, '1234567');
+    expect(inputPassword).toHaveValue('1234567');
+  });
+});
+
+describe('5 - Verificar se o botão está desativado ao renderizar a tela Login', () => {
+  it('Será validado se o botão será ativado após preenchimento dos campos', () => {
+    render(<Login />);
+
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const submitButton = screen.getByTestId(SUBMIT_BUTTON);
+
+    expect(submitButton).toBeDisabled();
+    userEvent.type(inputEmail, 'teste@teste');
+    expect(submitButton).toBeDisabled();
+    userEvent.type(inputEmail, 'teste@teste.com');
+    userEvent.type(inputPassword, '1234567');
+    expect(submitButton).not.toBeDisabled();
   });
 });
