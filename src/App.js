a9a5e4e7680c32233191fe35c167a459f17e465e
Wendy Silva <silva.wendy.souza@gmail.com>
2022-03-29 16:30:14 -0300

    req 10: criando as rotas no app- completa

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ function App() {
           <Route exact path="/" component={ Login } />
           <Route exact path="/foods" component={ Foods } />
           <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/foods/:id" component={ FoodsDetails } />
+          <Route exact path="/drinks/:id" component={ DrinksDetails } />
+          <Route exact path="/foods/:id/in-progress" component={ FoodsInProgress } />
+          <Route exact path="/drinks/:id/in-progress" component={ DrinksInProgress } />
           <Route exact path="/explore" component={ Explore } />
           <Route exact path="/explore/foods" component={ ExploreFoods } />
           <Route exact path="/explore/drinks" component={ ExploreDrinks } />
