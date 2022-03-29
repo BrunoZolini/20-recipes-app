3f38277abacb8ff4986d99f5ca7dcb16475cb450
Wendy Silva <silva.wendy.souza@gmail.com>
2022-03-29 18:59:50 -0300

    req 11: redirecionamento para pagina de perfil  ao clicar no botão

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ title, profile, search }) {
+  const history = useHistory();
   return (
     <div>
       {profile && (
         <button
           type="button"
+          onClick={ () => { history.push('/profile'); } }
         >
           <img
             data-testid="profile-top-btn"
