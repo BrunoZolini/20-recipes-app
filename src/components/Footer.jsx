241d2001869f23fa61c04b43924be377e53569a3
Wendy Silva <silva.wendy.souza@gmail.com>
2022-03-31 14:59:11 -0300

    req 22/23/24: redirecionamento para pag dfoods, drinks e explore

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import '../styles/Footer.css';
 
 export default function Footer() {
+  const history = useHistory();
   return (
     <footer data-testid="footer">
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
         <img
           data-testid="drinks-bottom-btn"
           alt="drinkIcon"
           src={ drinkIcon }
         />
       </button>
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/explore') }
+      >
         <img
           data-testid="explore-bottom-btn"
           alt="exploreIcon"
           src={ exploreIcon }
         />
       </button>
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/foods') }
+      >
         <img
           data-testid="food-bottom-btn"
           alt="mealIcon"
