c5117c6214581e6d78472dd3f721139ecb0051ca
viniciusopassos <viniciusopassos@hotmail.com>
2022-03-31 15:41:32 -0300

    req-25/26: aparecer 12 cards na tela principal

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Header from '../components/Header';
 import RecipesCards from '../components/RecipesCards';
+import fetchAPI from '../service/API';
+import context from '../context/myContext';
 
 export default function Drinks() {
+  const { searchValue, setSearchValue } = useContext(context);
+
+  useEffect(() => {
+    const requestAPI = async () => {
+      const data = await fetchAPI('', 'name', 'Drinks');
+      setSearchValue({ ...searchValue, data });
+    };
+    requestAPI();
+  }, []);
+
   return (
     <div>
       <Header title="Drinks" search profile />
       <RecipesCards searchType="drinks" />
-
     </div>
   );
 }
