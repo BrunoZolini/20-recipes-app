8f4868599d997ef47f82f2965a5ce8297c1ca87f
Wendy Silva <silva.wendy.souza@gmail.com>
2022-03-31 14:55:14 -0300

    req 21: footer nas telas Explore e Profile

--- src/pages/ExploreFoodsNationalities.jsx
+++ src/pages/ExploreFoodsNationalities.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function ExploreFoodsNationalities() {
   return (
     <div>
       <Header title="Explore Nationalities" search profile />
+      <Footer />
     </div>
   );
 }
