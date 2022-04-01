25605de5fd09fe95d01059c795711f1477257cb9
viniciusopassos <viniciusopassos@hotmail.com>
2022-04-01 11:40:01 -0300

    req-44: ícone favorito da comida

--- src/components/RecipesDetails.jsx
+++ src/components/RecipesDetails.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function RecipesDetails({ withVideo }) {
+  const [favorite, setFavorite] = useState(false);
   return (
     <div>
       <img data-testid="recipe-photo" src="" alt="" />
@@ -9,9 +12,28 @@ export default function RecipesDetails({ withVideo }) {
       <button type="button" data-testid="share-btn">
         compartilhar
       </button>
-      <button type="button" data-testid="favorite-btn">
-        Favorito
-      </button>
+      <div>
+        <button
+          type="button"
+          onClick={ () => {
+            setFavorite(!favorite);
+          } }
+        >
+          {!favorite ? (
+            <img
+              data-testid="favorite-btn"
+              alt="whiteHeartIcon"
+              src={ whiteHeartIcon }
+            />
+          ) : (
+            <img
+              data-testid="favorite-btn"
+              alt="blackHeartIcon"
+              src={ blackHeartIcon }
+            />
+          )}
+        </button>
+      </div>
       <p data-testid="recipe-category">text</p>
       <p data-testid="0-ingredient-name-and-measure"> </p>
       <p data-testid="instructions">instructions</p>
--- src/pages/FoodsDetails.jsx
+++ src/pages/FoodsDetails.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import RecipesDetails from '../components/RecipesDetails';
+// import fetchAPI from '../service/API';
 
 export default function FoodsDetails() {
+  // useEffect(() => {
+  //   async function getData() {
+  //     const res = await fetchAPI(id, 'ingredient', 'Foods');
+  //     const data = await res.json();
+  //   }
+  //   getData();
+  // }, []);
+
   return (
     <div>
       <RecipesDetails withVideo />
