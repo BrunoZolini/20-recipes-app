f98ad03dd02242e367f11f7364878196e4f5964b
Bruno Zolini <bruno.zolini@hotmail.com>
2022-04-11 20:49:24 -0300

    style: Finish CSS

--- src/components/FilterRecipesCards.jsx
+++ src/components/FilterRecipesCards.jsx
@@ -6,6 +6,7 @@ import ShareButton from './ShareButton';
 import '../styles/RecipesCards.css';
 import FavoriteButton from './FavoriteButton';
 import { deleteFavoriteRecipe, getFavoriteRecipes } from '../service/localStorage';
+import '../styles/DoneFavoriteCards.css';
 
 export default function FilterRecipesCards({ isFavorite }) {
   const { filterRecipes, setFilterRecipes } = useContext(context);
@@ -18,42 +19,65 @@ export default function FilterRecipesCards({ isFavorite }) {
   return (
     <div>
       {filterRecipes && filterRecipes.map((item, index) => (
-        <div className="cards-container" key={ item.id }>
+        <div className="done-favorite-container" key={ item.id }>
           <Link
-            className="card-display"
+            className="done-favorite-link"
             to={ `/${item.type}s/${item.id}` }
           >
             <img
-              className="card-img"
+              className="done-favorite-img"
               data-testid={ `${index}-horizontal-image` }
               src={ item.image }
               alt={ item.name }
             />
-            <p data-testid={ `${index}-horizontal-name` }>{ item.name }</p>
+            <div className="done-favorite-textarea">
+              <p
+                className="done-favorite-name"
+                data-testid={ `${index}-horizontal-name` }
+              >
+                { item.name }
+              </p>
+              <p
+                className="done-favorite-category"
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { item.type === 'food'
+                  ? `${item.nationality} - ${item.category}` : item.alcoholicOrNot }
+              </p>
+
+              { !isFavorite && (
+                <p
+                  className="done-favorite-date"
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  { `Done in: ${item.doneDate}` }
+                </p>)}
+
+              <div className="done-favorite-tag-container">
+                { !isFavorite && item.tags.map((tag, i) => (
+                  <p
+                    className="done-favorite-tag"
+                    key={ i }
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                  >
+                    {tag}
+                  </p>
+                ))}
+              </div>
+            </div>
           </Link>
-          <p data-testid={ `${index}-horizontal-top-text` }>
-            { item.type === 'food'
-              ? `${item.nationality} - ${item.category}` : item.alcoholicOrNot }
-          </p>
-          <p data-testid={ `${index}-horizontal-done-date` }>{ item.doneDate }</p>
-          { !isFavorite && item.tags.map((tag, i) => (
-            <p
-              key={ i }
-              data-testid={ `${index}-${tag}-horizontal-tag` }
-            >
-              {tag}
-            </p>
-          ))}
-          <ShareButton
-            type={ `${item.type}s` }
-            id={ item.id }
-            datatest={ `${index}-horizontal-share-btn` }
-          />
-          { isFavorite && <FavoriteButton
-            favorite
-            handleFavoriteButton={ () => handleFavoriteButton(item.id) }
-            datatest={ `${index}-horizontal-favorite-btn` }
-          /> }
+          <div className="done-favorite-button">
+            <ShareButton
+              type={ `${item.type}s` }
+              id={ item.id }
+              datatest={ `${index}-horizontal-share-btn` }
+            />
+            { isFavorite && <FavoriteButton
+              favorite
+              handleFavoriteButton={ () => handleFavoriteButton(item.id) }
+              datatest={ `${index}-horizontal-favorite-btn` }
+            /> }
+          </div>
         </div>
       ))}
     </div>
